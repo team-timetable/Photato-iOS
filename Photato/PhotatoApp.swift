f96f2acd484a571d2bbac3a471c7022b9cb2c6cb
jiyun0220 <145185712+jiyun0220@users.noreply.github.com>
2024-09-20 09:42:02 +0900

    feat:: first

---
+++ Photato/PhotatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotatoApp.swift
+//  Photato
+//
+//  Created by dgsw01 on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
